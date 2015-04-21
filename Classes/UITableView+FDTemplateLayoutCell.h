4e07406913939b56c8486277b225f583b86c0861
Yuan Sun <sunyuan1713@gmail.com>
2015-04-21 10:34:28 +0800

    Copyright -> 2016

--- Classes/UITableView+FDTemplateLayoutCell.h
+++ Classes/UITableView+FDTemplateLayoutCell.h
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2015-1016 forkingdog ( https://github.com/forkingdog )
+// Copyright (c) 2015-2016 forkingdog ( https://github.com/forkingdog )
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
