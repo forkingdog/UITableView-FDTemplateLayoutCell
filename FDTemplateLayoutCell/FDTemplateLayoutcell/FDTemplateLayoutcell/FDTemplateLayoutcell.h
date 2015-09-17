4fd3edc7f2687711f04dab8d10ac05228599fb06
ospreyren <58637717@qq.com>
2015-09-17 17:44:05 +0800

    Support Carthage

---
+++ FDTemplateLayoutCell/FDTemplateLayoutcell/FDTemplateLayoutcell/FDTemplateLayoutcell.h
@@ -0,0 +1,19 @@
+//
+//  FDTemplateLayoutcell.h
+//  FDTemplateLayoutcell
+//
+//  Created by ospreyren on 9/17/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FDTemplateLayoutcell.
+FOUNDATION_EXPORT double FDTemplateLayoutcellVersionNumber;
+
+//! Project version string for FDTemplateLayoutcell.
+FOUNDATION_EXPORT const unsigned char FDTemplateLayoutcellVersionString[];
+
+#import <FDTemplateLayoutcell/UITableView+FDTemplateLayoutCell.h>
+
+
